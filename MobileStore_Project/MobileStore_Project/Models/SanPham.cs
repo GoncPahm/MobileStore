1850b19e2abba24233ea931be178be85aa0da337
kain-1910 <pvcong1910@gmail.com>
2024-08-17 10:37:17 +0700

    upload project

---
+++ MobileStore_Project/MobileStore_Project/Models/SanPham.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MobileStore_Project.Models
+{
+    public partial class SanPham
+    {
+        public SanPham()
+        {
+            DonHangs = new HashSet<DonHang>();
+        }
+
+        public int MaSp { get; set; }
+        public string MaNsx { get; set; } = null!;
+        public int? SoLuong { get; set; }
+        public string? TenSp { get; set; }
+        public string? CauHinh { get; set; }
+        public string? MoTa { get; set; }
+        public string? PhienBan { get; set; }
+        public int? KhuyenMai { get; set; }
+        public string? MauSac { get; set; }
+        public decimal? Giaban { get; set; }
+        public string? AnhSp { get; set; }
+
+        public virtual Nsx MaNsxNavigation { get; set; } = null!;
+        public virtual ICollection<DonHang> DonHangs { get; set; }
+    }
+}
